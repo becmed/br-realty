1be7e85432b9dff8f2bddfb7a2a01b514ec47c27
becmed <buchingermayer@gmail.com>
2026-04-26 17:44:11 -0400

    Update portal.js

--- portal.js
+++ portal.js
@@ -1,1 +1,19 @@
+import { auth } from "./firebase-config.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
+const loginForm = document.getElementById("loginForm");
+const message = document.getElementById("message");
+
+loginForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    window.location.href = "dashboard.html";
+  } catch (error) {
+    message.textContent = error.message;
+  }
+});
