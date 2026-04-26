8c9e325c0ca1240e762efd7a7be58153e4044888
becmed <buchingermayer@gmail.com>
2026-04-26 17:50:14 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,21 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDT_8kc5fXZ-qZfY4UJV9sNj3Mm8X36E00",
+  authDomain: "br-realty-portal.firebaseapp.com",
+  projectId: "br-realty-portal",
+  storageBucket: "br-realty-portal.firebasestorage.app",
+  messagingSenderId: "1013054742843",
+  appId: "1:1013054742843:web:28b5bc5acc9fa58641d71e",
+  measurementId: "G-3VRPGH0LXW"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+const analytics = getAnalytics(app);
